569c4fccc1bd189e9cd7ae2b44ad10435f2e113f
Rajasekharreddy <144523570+Raj00005@users.noreply.github.com>
2024-03-18 10:27:33 +0530

    Delete assets directory

--- assets/vendor/venobox/venobox.js
+++
@@ -1,9 +0,0 @@
-/*
- * VenoBox - jQuery Plugin
- * version: 1.9.1
- * @requires jQuery >= 1.7.0
- *
- * Examples at http://veno.es/venobox/
- * License: MIT License
- * License URI: https://github.com/nicolafranchini/VenoBox/blob/master/LICENSE
- * Copyright 2
